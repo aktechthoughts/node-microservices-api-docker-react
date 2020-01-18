9093a7e09bfe715f05008acb2e277c343983ea4a
Abhishek Kumar <abhishek.Kumar01@externals.adidas.com>
2020-01-18 16:27:20 +0100

    Initial Express

---
+++ src/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var app = express();
+
+app.listen(3000,function(){
+    console.log('listening')
+
+});
+
+
+/* const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); */
