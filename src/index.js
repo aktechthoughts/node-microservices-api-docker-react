b8cc701b8a74733eed9143b248cc57fcd3fb3c28
Abhishek Kumar <abhishek.Kumar01@externals.adidas.com>
2020-01-18 16:52:53 +0100

    Simple node app on Docker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
 
 app.listen(3000,function(){
-    console.log('listening')
-
+    console.log('listening on port:'+3000)
 });
 
 
