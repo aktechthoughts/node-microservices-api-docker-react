9d47b6b652d26784ac1de256f0c4bfe0a0668f0b
Abhishek Kumar <abhishek.Kumar01@externals.adidas.com>
2020-01-19 14:29:47 +0100

    update

--- server.js
+++ server.js
@@ -9,16 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-<<<<<<< HEAD:src/server.js
-  //res.send('Hello World');
-    res.sendfile('index.html')
-});
-
-app.get('/',function(req,res) {
-  res.sendFile('index.html');
-=======
   res.send('Hello World');
->>>>>>> parent of d155257a... Update:server.js
 });
 
 app.listen(PORT, HOST);
