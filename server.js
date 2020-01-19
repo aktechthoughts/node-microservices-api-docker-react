d155257a85dc393dc37c95be6d50e81b46a25206
Abhishek Kumar <abhishek.Kumar01@externals.adidas.com>
2020-01-19 10:53:52 +0100

    Update

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const express = require('express');
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+
+app.get('/',function(req,res) {
+  //res.send('Hello World');
+  res.sendFile('index.html');
 });
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
